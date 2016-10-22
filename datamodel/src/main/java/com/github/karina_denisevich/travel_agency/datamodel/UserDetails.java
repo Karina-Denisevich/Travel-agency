91c484350726ee66cebd1415a7929ac68d4f6ece
Karina Denisevich <karina_41997@mail.ru>
2016-10-23 00:00:18 +0300

    master: UserDetails class added

---
+++ datamodel/src/main/java/com/github/karina_denisevich/travel_agency/datamodel/UserDetails.java
@@ -0,0 +1,61 @@
+package com.github.karina_denisevich.travel_agency.datamodel;
+
+import java.util.Date;
+
+public class UserDetails extends AbstractModel {
+
+    private String firstName;
+    private String lastName;
+    private Double discount;
+    private Date bDate;
+    private String phone;
+    private String skype;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Date getbDate() {
+        return bDate;
+    }
+
+    public void setbDate(Date bDate) {
+        this.bDate = bDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSkype() {
+        return skype;
+    }
+
+    public void setSkype(String skype) {
+        this.skype = skype;
+    }
+}

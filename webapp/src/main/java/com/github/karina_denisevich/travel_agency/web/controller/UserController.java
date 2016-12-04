918c3c2d3dc5eea90d9533b14d1c6d3233440baa
Karina Denisevich <karina_41997@mail.ru>
2016-12-04 14:55:18 +0300

    master: user controller refactored

--- webapp/src/main/java/com/github/karina_denisevich/travel_agency/web/controller/UserController.java
+++ webapp/src/main/java/com/github/karina_denisevich/travel_agency/web/controller/UserController.java
@@ -9,10 +9,7 @@ import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -28,20 +25,25 @@ public class UserController extends AbstractController<User, UserDto, Long> {
     @Inject
     private ConversionServiceFactoryBean conversionService;
 
-    @RequestMapping(value = "/getByEmail/{email}", method = RequestMethod.GET)
-    public ResponseEntity<UserDto> getByEmail(@PathVariable String email) {
+    @RequestMapping(value = "/search", method = RequestMethod.GET, params = "email")
+    public ResponseEntity<UserDto> getByEmail(String email) {
         return new ResponseEntity<>(conversionService.getObject()
                 .convert(userService.getByEmail(email), UserDto.class), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/getWithRole/{userId}", method = RequestMethod.GET)
-    public ResponseEntity<UserDto> getWithRoleById(@PathVariable Long userId) {
-        return new ResponseEntity<>(conversionService.getObject()
-                .convert(userService.getWithRole(userId), UserDto.class), HttpStatus.OK);
+    @RequestMapping(value = "/{userId}", params = "role", method = RequestMethod.GET)
+    public ResponseEntity<UserDto> getWithRoleById(@PathVariable Long userId,
+                                                   @RequestParam(value = "role", defaultValue = "false") Boolean isWithRole) {
+        if (isWithRole) {
+            return new ResponseEntity<>(conversionService.getObject()
+                    .convert(userService.getWithRole(userId), UserDto.class), HttpStatus.OK);
+        } else {
+            return super.getById(userId);
+        }
     }
 
-    @RequestMapping(value = "/getByRole/{roleType}", method = RequestMethod.GET)
-    public ResponseEntity<List<UserDto>> getByRole(@PathVariable String roleType) {
+    @RequestMapping(value = "/search", params = "roleType", method = RequestMethod.GET)
+    public ResponseEntity<List<UserDto>> getByRole(String roleType) {
         Role role = new Role();
         role.setType(Role.RoleEnum.valueOf(roleType));
 
@@ -55,11 +57,5 @@ public class UserController extends AbstractController<User, UserDto, Long> {
                         TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(UserDto.class)));
 
         return new ResponseEntity<>(convertedList, HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/getWithRoleByEmail/{email}", method = RequestMethod.GET)
-    public ResponseEntity<UserDto> getWithRoleByEmail(@PathVariable String email) {
-        return new ResponseEntity<>(conversionService.getObject()
-                .convert(userService.getByEmailWithRole(email), UserDto.class), HttpStatus.OK);
     }
 }
